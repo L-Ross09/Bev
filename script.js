caba8e71a5bb82f915bbf0127acd5b7b15997079
Luke73629 <labbott27@students.stab.org>
2024-12-13 09:42:38 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Science/Beverages%20Nutrition.csv";
+var names = getColumn(url, 1)
+var type = getColumn(url,2)
+var sugarFree = getColumn(url, 3)
+var calories = getColumn(url, 4)
+var fat = getColumn(url, 5)
+var protien = getColumn(url, 6)
+var carbs = getColumn(url, 7)
+var sugar = getColumn(url, 8)
+
+
+function results(input1, input2){
+
+    var macthingDrinks = [];
+ 
+    for(var i = 0; i <names.length; i++){
+        if (type[i] == input1 && sugarFree[i] == input2){
+            macthingDrinks.push(names[i])
+        }
+    }
+    console.log(macthingDrinks)
+}
+
