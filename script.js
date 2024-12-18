81864c9e1aeb6021b2efd646874e086c3d7ba685
Luke73629 <labbott27@students.stab.org>
2024-12-18 08:56:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,26 +3,37 @@ var names = getColumn(url, 1)
 var type = getColumn(url,2)
 var sugarFree = getColumn(url, 3)
 var calories = getColumn(url, 4)
-var fat = getColumn(url, 5)
-var protien = getColumn(url, 6)
-var carbs = getColumn(url, 7)
-var sugar = getColumn(url, 8)
 
 
-function results(input1, input2, input3){
+
+
+function results(typeInput, sugarfreeInput, caloriesInput){
 
     var macthingDrinks = [];
- 
+//  This is a loop that cycles through the dataset, if the drink in the dataset matches with the corresponding inputs then it adds that drink to the list.
+
     for(var i = 0; i <names.length; i++){
-        if (type[i] == input1 && sugarFree[i] == input2 && input3){
+        if (type[i] == typeInput && sugarFree[i] == sugarfreeInput && calories[i] < caloriesInput){
             macthingDrinks.push(names[i])
         }
     } 
-    console.log(macthingDrinks)
-    document.getElementById("output").innerHTML = macthingDrinks + "br";
+// if there are no corresponding drinks then an error message is displayed. 
+    if (macthingDrinks.length == 0 ){
+        document.getElementById("output").innerHTML = "Oh no, there are no drinks that meet this criteria.";
+    }
+    // if there is a corresponding drinks then they are displayed in the output box.
+    else {
+
+     
+        document.getElementById("output").innerHTML = "There are " + macthingDrinks.length + " results." + "<br>" + macthingDrinks.join("<br>");
+    
+    }
 
 }
-
+// This function sets the number under the slider to the calories value in the dataset.
 function showCalories(){
     document.getElementById("sliderValue").innerHTML = document.getElementById("calories").value;
 }
+
+
+// original source: "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Science/Beverages%20Nutrition.
